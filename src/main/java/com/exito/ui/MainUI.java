76537022ea5e0469ddb4b2325426c8f55d370daa
Ivan Morales <alternfourty@protonmail.com>
2024-07-07 17:26:39 -0500

    update xpath for UI elements

--- src/main/java/com/exito/ui/MainUI.java
+++ src/main/java/com/exito/ui/MainUI.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.By;
 
 public class MainUI {
     public static Target TXT_SEARCHBAR = Target.the("Search Field").located(By.xpath("//input[@aria-label='search']"));
-    public static Target LINK_TO_ITEM = Target.the("Name of the item").located(By.xpath("//a[@data-testid='product-link' and @title]"));
+    public static Target LINK_TO_ITEM = Target.the("Name of the item").located(By.xpath("//p[@class='styles_name__qQJiK']"));
     public static Target BTN_SHOPPINGCART = Target.the("Shopping Cart Icon").located(By.xpath("//button[@aria-label='Cart toggle button']"));
 }
--- src/main/java/com/exito/ui/SearchResultsUI.java
+++ src/main/java/com/exito/ui/SearchResultsUI.java
@@ -4,5 +4,5 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class SearchResultsUI {
-    public static Target BTN_ADDITEM = Target.the("Add to cart button").located(By.xpath("//button[@class='DefaultStyle_default__vCozi ']"));
+    public static Target BTN_ADDITEM = Target.the("Add to cart button").located(By.xpath("//button[@class='DefaultStyle_default__jW12W  ']"));
 }
